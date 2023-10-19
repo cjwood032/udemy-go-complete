01fce04c98dc28774a4736922e31b5abadb41554
Chris Wood <cjwood032@gmail.com>
2023-10-18 21:09:39 -0400

    added additional tests

--- 3_Deck/deck.go
+++ 3_Deck/deck.go
@@ -12,7 +12,7 @@ type deck []string
 
 func newDeck() deck {
 	cards := deck{}
-	suits := []string{"♠", "♥", "♦", "♣"}
+	suits := []string{"♠", "♦", "♣", "♥"}
 	values := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
 	for _, suit := range suits {
 		for _, value := range values {
--- 3_Deck/deck_test.go
+++ 3_Deck/deck_test.go
@@ -9,3 +9,26 @@ func TestNewDeckSize(t *testing.T) {
 		t.Errorf("Expected deck length of %v, but got %v", expected, len(d))
 	}
 }
+func TestNewDeckCards(t *testing.T) {
+	d := newDeck()
+	expectedStart := "A♠"
+	if d[0] != expectedStart {
+		t.Errorf("Expected first card of deck %v, but got %v", expectedStart, d[0])
+	}
+	expectedEnd := "K♥"
+	if d[len(d)-1] != expectedEnd {
+		t.Errorf("Expected last card of deck %v, but got %v", expectedEnd, d[len(d)-1])
+	}
+}
+func TestShuffledDeckCards(t *testing.T) {
+	d := newDeck()
+	s := newDeck()
+	//s.shuffle()
+	if len(d) != len(s) {
+		t.Errorf("Expected decks to be equal length, but got %v and %v", len(s), len(d))
+	}
+	if d[0] == s[0] && d[1] == s[1] && d[2] == s[2] {
+		//it's a 1/140608 chance to fail because all 3 cards ended up in the same spot
+		t.Errorf("Expected shuffled deck to be in a different order than a non-shuffled deck")
+	}
+}
